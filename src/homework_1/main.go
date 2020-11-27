2300f8b877d00f08c14e85335793ad284254864b
Mikhail Mir <mirzaakhmetov@infodev.ru>
2020-11-27 17:13:22 +1000

    Create func with print some text

---
+++ src/homework_1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func rotateSpinner() {
+	var spinner = [2]string{"♡", "♥"}
+	var count int
+	for {
+		fmt.Printf("\r  \033[36mWaiting next lesson ...\033[m %s ", spinner[count%2])
+		time.Sleep(time.Millisecond * 300)
+		count++
+	}
+}
+
+
+
+func main() {
+	rotateSpinner()
+}
