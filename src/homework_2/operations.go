ef57e23e5a327a9849a100d17d133ff95d80bf63
akaKAIN <mkain@mail.ru>
2020-12-02 07:37:17 +1000

    Create simple math functions

--- src/homework_2/operations.go
+++ src/homework_2/operations.go
@@ -1,1 +1,14 @@
 package main
+
+func add(numA, numB int) int {
+	return numA + numB
+}
+func sub(numA, numB int) int {
+	return numA - numB
+}
+func mul(numA, numB int) int {
+	return numA * numB
+}
+func div(numA, numB int) int {
+	return numA / numB
+}
