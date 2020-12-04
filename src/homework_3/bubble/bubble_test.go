e284701eba200a11fdb34e1f2a9515b77deaa71b
Mikhail Mir <mirzaakhmetov@infodev.ru>
2020-12-04 17:03:39 +1000

    fmt

--- src/homework_3/bubble/bubble_test.go
+++ src/homework_3/bubble/bubble_test.go
@@ -6,22 +6,22 @@ import (
 )
 
 func TestSort(t *testing.T) {
-	arr := []int{4,5,2,7,1}
-	expectRes := []int{1,2,4,5,7}
+	arr := []int{4, 5, 2, 7, 1}
+	expectRes := []int{1, 2, 4, 5, 7}
 	result := Sort(arr)
 	if !reflect.DeepEqual(result, expectRes) {
 		t.Fatalf("Expected: %v, gotted: %v\n", expectRes, result)
 	}
 
-	arr = []int{1,2,3,4,5}
-	expectRes = []int{1,2,3,4,5}
+	arr = []int{1, 2, 3, 4, 5}
+	expectRes = []int{1, 2, 3, 4, 5}
 	result = Sort(arr)
 	if !reflect.DeepEqual(result, expectRes) {
 		t.Fatalf("Expected: %v, gotted: %v\n", expectRes, result)
 	}
 
-	arr = []int{5,4,3,2,1}
-	expectRes = []int{1,2,3,4,5}
+	arr = []int{5, 4, 3, 2, 1}
+	expectRes = []int{1, 2, 3, 4, 5}
 	result = Sort(arr)
 	if !reflect.DeepEqual(result, expectRes) {
 		t.Fatalf("Expected: %v, gotted: %v\n", expectRes, result)
@@ -29,8 +29,8 @@ func TestSort(t *testing.T) {
 }
 
 func BenchmarkSort(b *testing.B) {
-	base := []int{5,4,3,6,9,10,7,2,1,8}
-	expect := []int{1,2,3,4,5,6,7,8,9,10}
+	base := []int{5, 4, 3, 6, 9, 10, 7, 2, 1, 8}
+	expect := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for i := 0; i < b.N; i++ {
 		arr := Sort(base)
 		if !reflect.DeepEqual(arr, expect) {
