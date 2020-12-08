55569ea652fb78cbee50194ef3517cd5bbe0c2e4
akaKAIN <mkain@mail.ru>
2020-12-08 20:29:50 +1000

    Corrected remarks

--- src/homework_3/bubble/bubble.go
+++ src/homework_3/bubble/bubble.go
@@ -4,7 +4,7 @@ func Sort(arr []int) []int {
 	// цикл пузыльковой сортировки
 	for {
 		done := true
-		for i := 0; i <= len(arr)-2; i++ {
+		for i := 0; i < len(arr)-1; i++ {
 			if arr[i] > arr[i+1] {
 				done = false
 				arr[i], arr[i+1] = arr[i+1], arr[i]
--- src/homework_3/insertion/insertion.go
+++ src/homework_3/insertion/insertion.go
@@ -1,7 +1,7 @@
 package insertion
 
 func Sort(arr []int) []int {
-	for i := 1; i <= len(arr)-1; i++ {
+	for i := 1; i < len(arr); i++ {
 		for j := i; j >= 1; j-- {
 			if arr[j-1] > arr[j] {
 				arr[j-1], arr[j] = arr[j], arr[j-1]
